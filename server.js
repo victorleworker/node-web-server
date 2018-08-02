92e3c72c55f73ad585610736499034a16d85c26e
le hoang viet <viet.le.worker@gmail.com>
2018-08-02 14:22:15 +0700

    Add prlject page

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use((req, res, next) => {
 });
 
 
-
+/*
 app.use((req, res, next) => {
     res.render('maintenance.hbs');
 })
-
+*/
 
 app.use(express.static(__dirname + '/public'));
 
@@ -50,6 +50,12 @@ app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About page'
     });
+});
+
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs', {
+        pateTitle: 'Projects'
+    })
 });
 
 ///bad -send back json with errormessage
